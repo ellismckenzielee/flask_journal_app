1db0d4c5861ab81bd457dc37e3cf258a48b87397
ellismckenzielee <ellismckenzielee@gmail.com>
2020-03-19 14:33:29 +0000

    added webbrowser module to load the browser automatically

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from journal_app import app
+import webbrowser
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port=5000
+    #url = 'localhost:{}'.format(port)
+    #webbrowser.open(url)
+    app.run(debug=False, port=port)
