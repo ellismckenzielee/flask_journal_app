f041108d8d3ea67f88770513f9293dc022e2c568
ellismckenzielee <ellismckenzielee@gmail.com>
2020-03-22 12:30:45 +0000

    added functionality to index route. gets all posts and displays them in index.html

--- journal_app/routes.py
+++ journal_app/routes.py
@@ -1,7 +1,9 @@
-from journal_app import app
+from journal_app import app, db
+from journal_app.models import Post
 from flask import render_template
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
     
